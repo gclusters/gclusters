e992d29b58efea8387c1e357dd43c38b9ab9927c
Marco Castellani <m.castellani@gmail.com>
2011-10-18 16:44:25 +0200

    pulizia commenti et al

--- details.php
+++ details.php
@@ -11,6 +11,8 @@ $param=$_GET['param'];
 
 $ggc=trim($ggc);
 
+// asking for the list of values for the selected parameter
+
 $query_auth = "SELECT * FROM newpar where cluster like '$ggc' and param like '$param'";
 $query_par = "SELECT * FROM parameters where ID like '$ggc'";
 
@@ -25,7 +27,7 @@ $numpar = mysql_num_rows($res_par);
 if (!$numres)
 {
 // Non ho trovato nulla, allora allargo i criteri di ricerca...
-// $query_auth = "SELECT * FROM newpar where cluster like '%$ggc%' ORDER BY param";
+
 $query_auth = "SELECT * FROM newpar where cluster like '%$ggc%' and param like '$param'";
 $query_par = "SELECT * FROM parameters where ID like '%$ggc%'";
 
@@ -34,8 +36,6 @@ $res_par = mysql_query($query_par) or die("Query failed");
 
 $numres = mysql_num_rows($result);
 }
-
-// $row = mysql_fetch_row($result);
 
 ?>
 
@@ -81,13 +81,11 @@ echo "<BODY background=\"back-012.gif\" TEXT=\"#0000FF\" LINK=\"#0000FF\" VLINK=
 
 include 'inte2.php';
 
-   echo '<h1>','<i>Cluster selected: </i>',$ggc,'</h1>';
-// echo '<h1>','<i>Cluster selected: </i>',$line[1],'</h1>';
+echo '<h1>','<i>Cluster selected: </i>',$ggc,'</h1>';
 echo "\n";
 
    $numres++;
    if ($numres>0){
-//   print "<h2>The following $numres parameters are in the database.</h2><p>\n";
    print "<h3>The following values are in the database:</h3><p>\n";
    print "<table border=1 width=40%>\n";
    print "\t\t<tr bgcolor=\"yellow\"><td>Parameter</td><td>Value</td><td>Source</td><td>Year</td></tr><tr>";
@@ -97,10 +95,10 @@ echo "\n";
   while ($line = mysql_fetch_row($result)) {
 
    print "\t<tr>\n";
-   print "\t\t<td><i>".$col[$line[2]]."</i></td>\n";
-   print "\t\t<td><i>".$line[3]."</i></td>\n";
-   print '<td><a href="'.$line[6].'">'.$line[4].'</a></td>';
-   print "\t\t<td><i>".$line[5]."</i></td>\n";
+   print "\t\t<td><i>".$col[$line[2]]."</i></td>\n";			// Parameter
+   print "\t\t<td><i>".$line[3]."</i></td>\n";					// Value
+   print '<td><a href="'.$line[6].'">'.$line[4].'</a></td>';	// Source
+   print "\t\t<td><i>".$line[5]."</i></td>\n";					// Year
    print "\t</tr>\n";
 
    $parid=$col[$line[2]]; // sporco! Da modificare: mi serve anche per solo Harris...
@@ -108,13 +106,10 @@ echo "\n";
    }
 
 // Valore dal catalogo di Harris....
-// print "</table><p>\n";
 
 $linepar = mysql_fetch_row($res_par);
 $numpar = mysql_num_rows($res_par);
 
-//print "<table border=1 width=40%><tr>\n";
-// print "\t\t\<td>Parameter</td><td>Value</td><td>Source</td><td>Year</td></tr><tr>";
 print "\t\t<td><i>".$col[$param]."</i></td>\n";
 print "\t\t<td><i>".$linepar[$param]."</i></td>\n";
 print '<td><i><a href="http://www.physics.mcmaster.ca/Globular.html">Harris Catalogue</a></i></td>';
@@ -138,42 +133,7 @@ exit;
 
 }
 
-
-/*
-// Contatore degli accessi
-
-/*
-$query_n= "SELECT * from accesscount WHERE ID = '$row[0]'";
-$lres_n = mysql_query($query_n) or die("Query failed");
-
-// Controllo aggiornamento pagina
-
-$n_cluster=mysql_fetch_row($lres_n);
-
-$nn_cluster=$n_cluster[2];
-$cluster_update=$n_cluster[3];
-
-
-$n_cluster1 = $nn_cluster + 1;
-
-$query_n2 = "UPDATE accesscount SET n_vis = '$n_cluster1' WHERE ID = '$row[0]' LIMIT 1";
-$lltest=mysql_query($query_n2);
-echo "This page has been visited ";
-echo $n_cluster1;
-echo " times, since Oct 25 2004<br>";
-if ($cluster_update!=="0000-00-00")
-{
-echo "<p><i><img src=\"new_anim.gif\">";
-echo "Page updated: ";
-echo $cluster_update;
-echo "</i>";
-};
-
-include 'coda.html';
-
-*/
 ?>
-
 
 </body>
 </html>
