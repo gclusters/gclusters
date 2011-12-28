bc9138d124a68dbe134532753324a3850cb97a58
Marco Castellani <m.castellani@gmail.com>
2011-12-28 17:39:34 +0100

    Aggiunti filini connessioni e G+

---
+++ conn_xeround.php
@@ -0,0 +1,7 @@
+<?
+
+//$link = mysql_connect("localhost", "root", "root"); 
+//mysql_select_db("globularclusters");
+$link = mysql_connect("ec2-46-137-188-78.eu-west-1.compute.amazonaws.com:3603", "mcastel", "amarok84"); 
+mysql_select_db("gclusters");
+?>
