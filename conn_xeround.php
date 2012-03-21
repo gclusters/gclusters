91532d6473768683784582fe85bfe62a279ef34e
Marco Castellani <m.castellani@gmail.com>
2012-03-21 15:08:26 +0100

    Allineamento

--- conn_xeround.php
+++ conn_xeround.php
@@ -1,7 +1,4 @@
 <?
-
-//$link = mysql_connect("localhost", "root", "root"); 
-//mysql_select_db("globularclusters");
-$link = mysql_connect("ec2-46-137-188-78.eu-west-1.compute.amazonaws.com:3603", "mcastel", "amarok84"); 
-mysql_select_db("gclusters");
+$link = mysql_connect("instance5023.db.xeround.com:3569", "mcastel", "amarok"); 
+mysql_select_db("test");
 ?>
