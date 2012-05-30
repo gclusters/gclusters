0662f9669227d482914f80ffe979740a8f857913
Marco Castellani <m.castellani@gmail.com>
2012-05-30 11:11:34 +0200

    list of clusters in user page

--- user.php
+++ user.php
@@ -10,8 +10,16 @@ include 'conn.php';
 $id= $_GET['ID'];
 $id=trim($id);
 
+// **** SQL QUERIES ****
+
 $query="SELECT * from user where ID='$id'";
 $result = mysql_query($query) or die("Query failed");
+
+$qcl = "select tutors.cluster from tutors,user where tutors.person=user.ID and user.ID='$id'";
+$rescl = mysql_query($qcl) or die("Query failed");
+// $rrow = mysql_num_rows($qcl);
+
+// *** END OF SQL QUERIES ***
 
 ?>
 <h3>User details</h3>
@@ -28,12 +36,21 @@ if ($line[8]) echo "<tr><td>Web: <a href=\"$line[8]\">".$line[8].'</a></td></tr>
 if ($line[9]) echo "<tr><td>Facebook: <a href=\"$line[9]\">".$line[9].'</a></td></tr>';
 if ($line[10]) echo "<tr><td>Twitter: <a href=\"$line[9]\">".$line[10].'</a></td></tr>';
 
-
 ?>
 
 </table>
 
+<p>
+<i>
+<?php
+echo $line[3];
+echo ' is tutoring the following cluster(s):<br>';
 
+while ($ltut = mysql_fetch_array($rescl)){
+echo $ltut[0].'<br>';
+ }
+
+?>
 </body>
 </html>
 
