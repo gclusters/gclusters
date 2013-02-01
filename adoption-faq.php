7cd2885cdc2b3019e36d6623814c5829aadac073
Marco Castellani <m.castellani@gmail.com>
2013-02-01 22:02:23 +0100

    added adoption-faq.php

---
+++ adoption-faq.php
@@ -0,0 +1,60 @@
+<html>
+<head><title>GGCs database: "adoption" FAQ</title></head>
+<body background="backgr2.jpg" vlink="#3333CC">
+
+
+<?php include 'inte2.php'?>
+<center>
+<h2>Adoption FAQ page</h2>
+<h3>(Under construction...)
+</h3>
+</center>
+<br>
+<p>
+<table width="80%" align=center><tr><td>
+
+<b>Q:What information can I report to you, assuming I'm a tutor 
+	of <a href="cluster_4.php?ggc=NGC+104">NGC 104</a> (47 Tucanae)?</b> <br>
+<b>A:</b> Here are some examples of things you can report
+ (but you can easily extend to other cases as well)...
+
+<ul>
+<li>Milone et al, <a href="http://adsabs.harvard.edu/abs/2012ApJ...744...58M">Multiple Stellar Populations in 47 Tucanae</a>
+<i>(bibliographic item)</i></li>
+
+<li><a href="http://apod.nasa.gov/apod/ap110116.html">Globular Star Cluster 47 Tuc</a>
+<i> (link to a resource on the web)</i></li>
+
+<li>Unreddened distance modulus: 13.36 +/- 0.02 +/- 0.06 
+(source: <a href="http://adsabs.harvard.edu/abs/2012AJ....143...50W">Woodley et al.</a>) <i> (parameter value)</i>
+<li>...</li>
+</ul>
+
+<p>
+<b>Q: What happen if I do not send you any info for a long time?</b><br>
+<b>A:</b> If we have no activity from you for a whole year, we can think about removing your name on the given cluster,
+in order to free the slot for other person that may have more time (we'll send you a mail before doing any action)...
+
+
+<p>
+<b>Q: I've changed my mind! Can I resign from tutoring my cluster(s)?</b> <br>
+<b>A:</b> Anytime! Just drop us an email. :-)
+
+<p>
+<b>Q: I'm not sure, should I still wait?</b><br>
+<b>A:</b> No! There are still many clusters in need of a "tutor" <i>just now</i>... give a try. 
+In any case, you can always rely upon 
+the previous question... So, come on, <a href="form-adopt.php">adopt one of them!</a>
+<p>
+<b>Q: I've another question, but it's not listed here...</b><br>
+<b>A:</b> drop us an email to <i>gclusters@altervista.org</i>. Thanks in advance for your collaboration!
+
+</td></tr></table>
+
+
+<p>
+
+<?php include 'coda.html' ?>
+
+</body>
+</html>
