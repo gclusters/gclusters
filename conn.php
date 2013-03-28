cc4197a56507188d849522e69423f5fe9a26830e
Marco Castellani <m.castellani@gmail.com>
2013-03-28 10:17:31 +0100

    added some files to .gitignore

--- conn.php
+++ conn.php
@@ -4,6 +4,6 @@ $link = mysql_connect("localhost", "root", "root");
 mysql_select_db("globularclusters");
 
 ?>
-
-<center><b><i>you're on LocalHost, baby!</i></b></center> 
-
+<font color="blue">
+<center><b><i>you're on LocalHost, baby!</i></b></center><p> 
+</font>
