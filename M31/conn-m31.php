0968b6f551bf32e3a6b213a48e667b9720f1b442
Marco Castellani <m.castellani@gmail.com>
2013-05-14 17:50:35 +0200

    added first M31 files

---
+++ M31/conn-m31.php
@@ -0,0 +1,9 @@
+<?
+
+$link = mysql_connect("localhost", "root", "root");
+mysql_select_db("m31gcs");
+
+?>
+<font color="blue">
+    <center><b><i>you're on LocalHost, baby!</i></b></center><p>
+</font>
