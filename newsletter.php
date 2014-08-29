05550c28b6a3b3e73d9cfa9e4630def4be0e905a
Marco Castellani <m.castellani@gmail.com>
2014-08-29 10:33:08 +0200

    aggiunto file

---
+++ newsletter.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marco
+ * Date: 19/03/14
+ * Time: 17:54
+ */
+
+
+<script type="text/javascript"> var av_nl_lang = 'it'; </script>
+<script type="text/javascript" src="http://tl.altervista.org/js/newsletter.js"></script>
+
+
+?>
